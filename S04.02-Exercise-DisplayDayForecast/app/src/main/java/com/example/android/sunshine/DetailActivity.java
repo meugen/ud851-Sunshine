12522b206c7b8d1f91da5b807e90adbfb062aedf
meugen <meugen.82@gmail.com>
2017-03-19 22:37:20 +0200

    Implemented displaying weather in TextView

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
@@ -15,8 +16,11 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         // COMPLETED (2) Display the weather forecast that was passed from MainActivity
-        String weatherForDay = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-        Toast.makeText(this, weatherForDay + FORECAST_SHARE_HASHTAG,
-                Toast.LENGTH_SHORT).show();
+        Intent intent = getIntent();
+        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String weatherForDay = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+            TextView displayView = (TextView) findViewById(R.id.tv_display);
+            displayView.setText(weatherForDay + FORECAST_SHARE_HASHTAG);
+        }
     }
 }
