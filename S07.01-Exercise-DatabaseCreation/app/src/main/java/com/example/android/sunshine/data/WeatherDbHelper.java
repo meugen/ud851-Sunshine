c263cfe1e7531a3d91806b570b200dbb1bb50fd7
emalyshev <emalyshev@wellink.ru>
2017-04-12 09:58:40 +0300

    Implemented weather contract && DbHelper

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -15,19 +15,49 @@
  */
 package com.example.android.sunshine.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Manages a local database for weather data.
  */
-// TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
-public class WeatherDbHelper {
+// COMPLETED (11) Extend SQLiteOpenHelper from WeatherDbHelper
+public class WeatherDbHelper extends SQLiteOpenHelper {
 
-//  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+//  COMPLETED (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+    public static final String DATABASE_NAME = "weather.db";
 
-//  TODO (13) Create a private static final int called DATABASE_VERSION and set it to 1
+//  COMPLETED (13) Create a private static final int called DATABASE_VERSION and set it to 1
+    public static final int DATABASE_VERSION = 1;
 
-//  TODO (14) Create a constructor that accepts a context and call through to the superclass constructor
+//  COMPLETED (14) Create a constructor that accepts a context and call through to the superclass constructor
+    public WeatherDbHelper(final Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-//  TODO (15) Override onCreate and create the weather table from within it
+//  COMPLETED (15) Override onCreate and create the weather table from within it
+    @Override
+    public void onCreate(final SQLiteDatabase sqLiteDatabase) {
+        final String sql = "CREATE TABLE " +
+                WeatherContract.WeatherEntry.TABLE_NAME + " (" +
+                WeatherContract.WeatherEntry._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                WeatherContract.WeatherEntry.COLUMN_DATE + " TIMESTAMP NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_MAX_TEMP + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_HUMIDITY + " DOUBLE NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_PRESSURE + " DOUBLE NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_WIND_SPEED + " DOUBLE NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_DEGREES + " DOUBLE NOT NULL);";
+        sqLiteDatabase.execSQL(sql);
+    }
 
-//  TODO (16) Override onUpgrade, but don't do anything within it yet
+
+//  COMPLETED (16) Override onUpgrade, but don't do anything within it yet
+    @Override
+    public void onUpgrade(final SQLiteDatabase sqLiteDatabase,
+                          final int oldVersion, final int newVersion) {
+
+    }
 }
